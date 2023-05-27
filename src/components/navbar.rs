82b6ede799fb81c28aac9e05ad7ad5e70f1e9761
Eban Sayago <eban.sayago@protonmail.com>
2023-05-27 18:01:01 -0300

    :sparkles: Dark theme

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -9,6 +9,7 @@ use crate::counter_ctx::{CounterAction, CounterContext};
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
     let active = use_state(|| true);
+    let counter_ctx = use_context::<CounterContext>().unwrap();
 
     let toggle_active = {
         let active = active.clone();
@@ -25,7 +26,7 @@ pub fn navbar() -> Html {
         <header class={classes!(
             Some("px-3 py-6 rounded-b-3xl flex justify-between items-center"),
             active.then_some("bg-transparent"),
-            active.not().then_some("bg-white shadow-md"),
+            active.not().then_some("bg-white dark:bg-black dark:text-white shadow-md"),
         )}>
             <div class={classes!(
                 Some("text-5xl font-bold"),
@@ -41,14 +42,14 @@ pub fn navbar() -> Html {
                 }
             </button>
             <section class={classes!(
-                    Some("bg-white fixed z-10 inset-0 top-[96px] rounded-t-3xl shadow-md px-3 py-6"),
+                    Some("bg-white dark:bg-black dark:text-white fixed z-10 inset-0 top-[96px] rounded-t-3xl shadow-md px-3 py-6"),
                     active.not().then_some("hidden"),
             )}>
                 <h1 class="text-2xl font-bold">{"Settings"}</h1>
                 <ul class="max-w-md mx-auto">
                     <li class="flex justify-between items-center w-full">
                         <span>{"Dark mode"}</span>
-                        <input type="checkbox" onclick={toggle_dark_mode} />
+                        <input value={counter_ctx.dark_theme.to_string()} type="checkbox" onclick={toggle_dark_mode} />
                     </li>
                 </ul>
             </section>
