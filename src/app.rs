5c49221a92847a50d2666f4248f5c5c5bace0dbb
Eban Sayago <eban.sayago@protonmail.com>
2023-05-27 18:40:57 -0300

    :sparkles: Dim background when dark theme

--- src/app.rs
+++ src/app.rs
@@ -8,9 +8,11 @@ use crate::counter_ctx::CounterProvider;
 pub fn app() -> Html {
     html! {
         <CounterProvider>
-            <main class="min-h-screen w-screen">
+            <main class="min-h-screen w-screen bg-transparent">
                 <Navbar />
                 <CountList />
+                <div class="fixed inset-0 -z-[1] hidden dark:block bg-black bg-opacity-30"></div>
+                <div class="fixed inset-0 -z-[2] bg-gradient"></div>
             </main>
         </CounterProvider>
     }
